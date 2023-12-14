e4d48688823765a4afa842e4a9e7c85a2a757d23
anubhav300 <anubhavgupta.main@gmail.com>
2023-12-14 01:36:46 -0500

    Added a ListBox Created button functions

--- functions.py
+++ functions.py
@@ -1,5 +1,32 @@
+def add(s):
+    todos = list()
+    with open("todos.txt","r") as file:
+        todos = file.readlines()
+    todos.append(s)
+    with open("todos.txt","w") as file:
+        for element in todos:
+            element = element.strip()
+            file.write(element + '\n')
+
+def get_todos():
+    todo = list()
+    with open('todos.txt','r') as file:
+        todos = file.readlines()
+        for i in todos:
+            todo.append(i.strip())
+    return todo
+def edit():
+
+    print('helll')
+def complete():
+    print('helllo')
+
 def display(todos):
     with open("todos.txt", 'w') as file:
         for element in todos:
             element = element.strip('\n')
             file.write(element + '\n')
+
+
+if __name__ == '__main__':
+    print('This file should be used as a module')
--- gui.py
+++ gui.py
@@ -2,12 +2,23 @@ import functions
 import PySimpleGUI as sg
 
 label = sg.Text("Enter a to-do")
-inputLabel = sg.InputText(tooltip="Enter a to-do")
+inputLabel = sg.InputText(tooltip="Enter a to-do", key="todo", do_not_clear=False)
 add = sg.Button("ADD")
 complete = sg.Button("COMPLETE")
 edit = sg.Button("EDIT")
-window = sg.Window("TO-DO List", layout=[[label, inputLabel],[add,complete,edit]])
+list_box = sg.Listbox(values=functions.get_todos(), key = 'todos', enable_events=True, size=[45,10])
+window = sg.Window("TO-DO List", layout=[[label, inputLabel],[add,complete,edit],[list_box]], font=("Helvetica",13))
 
-window.read()
-
+while True:
+    event, value = window.read()
+    print(event, value)
+    match event:
+        case 'ADD':
+            if value['todo'].strip() == '':
+                print("Enter a valid todo")
+            else:
+                functions.add(value['todo'])
+                list_box.update(values= functions.get_todos())
+        case sg.WIN_CLOSED:
+            break
 window.close()
